ae19844094fdca265b3f1764fa7cab3edeb1c3e3
tkdking00 <musama4288921@gmail.com>
2024-10-10 04:48:39 -0700

    otherchanges

--- components/organism/Product.jsx
+++ components/organism/Product.jsx
@@ -87,128 +87,35 @@ console.log(filteredUniforms,"haelo :)");
 
   return (
     <>
-      <div className="container mx-auto p-4 flex">
-        {/* Sidebar Filters */}
-        <div className="w-1/4 p-4 bg-gray-200">
-          <h2 className="font-bold mb-4">Filters</h2>
-          <div className="flex flex-col">
-            <label>Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={filters.name}
-              onChange={handleFilterChange}
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label>Company:</label>
-            <input
-              type="text"
-              name="company"
-              value={filters.company}
-              onChange={handleFilterChange}
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label>Size:</label>
-            <input
-              type="text"
-              name="size"
-              value={filters.size}
-              onChange={handleFilterChange}
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label>Upper Color:</label>
-            <input
-              type="text"
-              name="upperColor"
-              value={filters.upperColor}
-              onChange={handleFilterChange}
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label>Trowser Color:</label>
-            <input
-              type="text"
-              name="trowserColor"
-              value={filters.trowserColor}
-              onChange={handleFilterChange}
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label>Seniority :</label>
-            <input
-              type="text"
-              name="seneiority"
-              value={filters.seneiority}
-              onChange={handleFilterChange}
-              className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-            />
-          </div>
-        </div>
 
-        {/* Main content */}
-        <div className="w-3/4">
-          {/* Form section */}
-          <FormSection
-            selectedUniform={selectedUniform}
-            onFormSubmit={handleFormSubmit}
-            className="form mb-6"
-            id="form"
-          />
-
-          {/* Loading indicator */}
-          {loading ? (
-            <SkeletonCards />
-          ) : (
-            <>
-              {/* Display total uniforms */}
-              <h1 className="text-center">Total: ({filteredUniforms.length})</h1>
-
-              {/* Uniforms list */}
-              <div className="p-4 mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {filteredUniforms.map((uniform) => (
-                  <ProductCard
-                    key={uniform._id}
-                    uniform={uniform}
-                    onEdit={handleEdit}
-                    onClick={() => handleCardClick(uniform)} // Pass the whole uniform object
-                  />
-                ))}
-              </div>
-            </>
-          )}
-        </div>
+    
+      <div className="relative  container mx-auto p-4 flex">
 
         {/* Image modal with object data */}
         {selectedImage && selectedUniformData && (
           <div
-            className="fixed inset-0 bg-white bg-opacity-75 flex items-center justify-center z-50"
+            className="absolute top-0 left-0 inset-0 bg-white flex items-center justify-center z-50  "
             onClick={handleCloseImage}
           >
+            <div className="py-[50px] flex items-center justify-center">
             <div className="grid grid-cols-2">
               <img
                 src={selectedImage}
                 alt="Full view"
-                className="  rounded-xl"
+                className=""
               />
-              <div className=" bg-gray-100 p-4 rounded shadow-md">
-                <h2 className="text-lg font-bold mb-2">
-                Company:  {selectedUniformData.company}
+              <div className=" bg-gray-100 p-4 rounded pl-[50px] pt-[50px]">
+                <h2 className="text-xl font-bold mb-2">
+                Company: <span className="text-md font-normal "> {selectedUniformData.company}</span>
                 </h2>
-                <p className="mb-2">Discaription: {selectedUniformData.name}</p>
-                <p className="mb-2">size: {selectedUniformData.size}</p>
-                <p>Color: {selectedUniformData.color}</p>
-                <p>Upper Color: {selectedUniformData.upperColor}</p>
-                <p>Trowser Color: {selectedUniformData.trowserColor}</p>
-                <p>Seneiority : {selectedUniformData.seneiority}</p>
+                <p className="mb-2  text-xl font-bold  ">Discaription: <span className="text-md font-normal">{selectedUniformData.name}</span></p>
+                <p className="mb-2 text-xl font-bold  ">Size:<span className="text-md font-normal"> {selectedUniformData.size}</span></p>
+                <p className="mb-2 text-xl font-bold">Color: <span className="text-md font-normal">{selectedUniformData.color}</span></p>
+                <p className="mb-2 text-xl font-bold">Upper Color: <span className="text-md font-normal">{selectedUniformData.upperColor}</span></p>
+                <p className="mb-2 text-xl font-bold">Trowser Color:<span className="text-md font-normal"> {selectedUniformData.trowserColor}</span></p>
+                <p className="mb-2 text-xl font-bold">Seneiority : <span className="text-md font-normal">{selectedUniformData.seneiority}</span></p>
               </div>
+            </div>
             </div>
           </div>
         )}
