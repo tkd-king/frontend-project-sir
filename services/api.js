963346d35cca08fee8170802319375143631fee3
tkdking00 <musama4288921@gmail.com>
2025-01-16 13:50:17 +0500

    new update

--- services/api.js
+++ services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`;
 
  const getUniforms = async ( filters, page ) => {
   try {
